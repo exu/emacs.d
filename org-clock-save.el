233606989f0e02434d3d1f5df444b9b3048b7342
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-03-23 17:11:36 +0100

    README update

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - battleaxe at <2013-03-22 pią 22:57>
+;; org-persist.el - battleaxe at <2013-03-23 sob 07:50>
 (setq stored-clock-history '(("/home/exu/org/work/todo.org" . 3422) ("/home/exu/org/work/todo.org" . 3440) ("/home/exu/org/work/todo.org" . 3583) ("/home/exu/org/work/todo.org" . 3729) ("/home/exu/org/work/todo.org" . 3808)))
