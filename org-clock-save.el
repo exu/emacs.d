70d29f4160b78bfdf5e1f9684102a0bfeeb70f8a
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-05-13 13:26:59 +0200

    variuos stuff

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - katana at <2013-05-07 wto 09:20>
-(setq stored-clock-history '(("/home/exu/org/work/todo.org" . 4495)))
+;; org-persist.el - katana at <2013-05-10 pią 19:15>
+(setq stored-clock-history '(("/home/exu/org/work/todo.org" . 3837) ("/home/exu/org/work/todo.org" . 4053) ("/home/exu/org/work/todo.org" . 4507) ("/home/exu/org/work/todo.org" . 4790) ("/home/exu/org/work/todo.org" . 4946)))
