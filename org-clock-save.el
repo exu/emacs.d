596918079d9249cb1ccaf8f4561bdca4bef6c317
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-05-22 13:35:09 +0200

    Move it to ignore someday

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - katana at <2013-05-20 pon 16:22>
-(setq stored-clock-history '(("/home/exu/org/work/todo.org" . 6908)))
+;; org-persist.el - katana at <2013-05-22 śro 08:51>
+(setq stored-clock-history '(("/home/exu/org/work/todo.org" . 6908) ("/home/exu/org/work/todo.org" . 8192)))
