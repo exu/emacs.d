f3e8b9ac2be490b84b4560d752776eb5f74a01a4
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-05-22 13:35:24 +0200

    Webjump google translate

---
+++ autoload/webjump.el
@@ -0,0 +1,14 @@
+(eval-after-load "webjump"
+'(progn (add-to-list 'webjump-sites
+              '("Translate EN-PL" .
+                [simple-query
+                 "translate.google.pl"
+                 "http://translate.google.pl/#en/pl/"
+                 ""]))
+        (add-to-list 'webjump-sites
+              '("Translate PL-EN" .
+                [simple-query
+                 "translate.google.pl"
+                 "http://translate.google.pl/#pl/en/"
+                 ""]))
+        ))
