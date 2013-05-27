6e43d37111bff34c5169885554ad57ef4a57c017
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-05-27 12:30:26 +0200

    some nasty stuff

--- autoload/webjump.el
+++ autoload/webjump.el
@@ -1,14 +1,14 @@
-(eval-after-load "webjump"
-'(progn (add-to-list 'webjump-sites
-              '("Translate EN-PL" .
-                [simple-query
-                 "translate.google.pl"
-                 "http://translate.google.pl/#en/pl/"
-                 ""]))
-        (add-to-list 'webjump-sites
-              '("Translate PL-EN" .
-                [simple-query
-                 "translate.google.pl"
-                 "http://translate.google.pl/#pl/en/"
-                 ""]))
-        ))
+;; (eval-after-load "webjump"
+;; '(progn (add-to-list 'webjump-sites
+;;               '("Translate EN-PL" .
+;;                 [simple-query
+;;                  "translate.google.pl"
+;;                  "http://translate.google.pl/#en/pl/"
+;;                  ""]))
+;;         (add-to-list 'webjump-sites
+;;               '("Translate PL-EN" .
+;;                 [simple-query
+;;                  "translate.google.pl"
+;;                  "http://translate.google.pl/#pl/en/"
+;;                  ""]))
+;;         ))
--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - katana at <2013-05-22 śro 08:51>
-(setq stored-clock-history '(("/home/exu/org/work/todo.org" . 6908) ("/home/exu/org/work/todo.org" . 8192)))
+;; org-persist.el - katana at <2013-05-24 pią 16:27>
+(setq stored-clock-history '(("/home/exu/org/work/todo.org" . 7211) ("/home/exu/org/work/todo.org" . 8496) ("/home/exu/org/work/todo.org" . 8779) ("/home/exu/org/work/todo.org" . 9233) ("/home/exu/org/work/todo.org" . 9014)))
