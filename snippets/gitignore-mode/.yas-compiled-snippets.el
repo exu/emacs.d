7af9de48014ca11236bdd59b0820bb9db8cb9ee3
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-05-27 12:28:07 +0200

    gitignore mode

---
+++ snippets/gitignore-mode/.yas-compiled-snippets.el
@@ -0,0 +1,10 @@
+;;; Compiled snippets and support files for `gitignore-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'gitignore-mode
+                     '(("tpl" "# Bootstrap\napp/bootstrap*\n\n# Symfony directories\nvendor/*\n*/logs/*\n*/cache/*\nweb/uploads/*\nweb/bundles/*\n\n# Configuration files\napp/config/parameters.yml\n\n# VIM, Emacs like editors tags file\nTAGS\ntags\n.ctags" "Ignore template" nil
+                        ("default")
+                        nil nil nil nil)))
+
+
+;;; Do not edit! File generated at Fri May 24 11:19:44 2013
