04a1f4cd1f5bc92a37603cd7e78d172c25a0b5d1
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-06-05 08:33:19 +0200

    order

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - katana at <2013-05-24 pią 16:27>
-(setq stored-clock-history '(("/home/exu/org/work/todo.org" . 7211) ("/home/exu/org/work/todo.org" . 8496) ("/home/exu/org/work/todo.org" . 8779) ("/home/exu/org/work/todo.org" . 9233) ("/home/exu/org/work/todo.org" . 9014)))
+;; org-persist.el - battleaxe at <2013-06-04 wto 21:56>
+(setq stored-clock-history '(("/home/exu/org/work/todo.org" . 3845)))
