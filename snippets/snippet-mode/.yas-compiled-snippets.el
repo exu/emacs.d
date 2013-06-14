9e12d619e35876ac148efe4ae59f0da68956e818
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-06-14 12:54:05 +0200

    Remove .yas-compiled-snippets.el from repo

--- snippets/snippet-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `snippet-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'snippet-mode
-                     '(("tpl" "# -*- mode: snippet -*-\n#contributor : Jacek Wysocki <jacek.wysocki@gmail.com>\n#name : $1\n#key : $2\n#group : ${3:default}\n# --\n$4\n" "Snippet template" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Thu Jun  6 08:15:11 2013
--- snippets/sql-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `sql-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'sql-mode
-                     '(("insert" "INSERT INTO $1 ($2) VALUES ($3);\n" "insert" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Thu Jun  6 08:15:11 2013
