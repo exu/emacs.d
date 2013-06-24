9e3c81c70afc10615793d11eda8632c71c775e24
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-06-24 09:40:35 +0200

    namespace snippet

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-persist.el - katana at <2013-06-18 wto 15:56>
-(setq stored-clock-history '(("/home/exu/org/work/todo.org_archive" . 214338) ("/home/exu/www/qar.sn/todo.org_archive" . 83)))
+;; org-persist.el - katana at <2013-06-21 pią 16:05>
+(setq stored-clock-history '(("/home/exu/org/work/todo.org_archive" . 214338) ("/home/exu/org/work/todo.org" . 3128)))
