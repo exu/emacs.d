863c5b01e5494108504ea3196593a26a505be0ee
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-10-01 11:11:22 +0200

    removed unused tern from js

--- autoload/javascript.el
+++ autoload/javascript.el
@@ -2,15 +2,15 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
-(add-to-list 'load-path "~/.emacs.d/vendor/tern/emacs/")
-(autoload 'tern-mode "tern.el" nil t)
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
-(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+;; (add-to-list 'load-path "~/.emacs.d/vendor/tern/emacs/")
+;; (autoload 'tern-mode "tern.el" nil t)
+;; (add-hook 'js-mode-hook (lambda () (tern-mode t)))
+;; (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 
-(eval-after-load 'tern
-   '(progn
-      (require 'tern-auto-complete)
-      (tern-ac-setup)))
+;; (eval-after-load 'tern
+;;    '(progn
+;;       (require 'tern-auto-complete)
+;;       (tern-ac-setup)))
 
 
 (require 'coffee-mode)
