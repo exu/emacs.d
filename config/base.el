e59275f8e7fbbfef58cbe5264ebebcdea936b8e0
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-10-01 11:13:10 +0200

    user email address is taken for new emails

--- config/base.el
+++ config/base.el
@@ -1,4 +1,5 @@
 (require 'dash)
 
 (fset 'yes-or-no-p 'y-or-n-p)
-(setq user-mail-address "jacek.wysocki@gmail.com")
+(setq user-mail-address "jacek.wysocki@edpauto.com")
+(setq user-full-name "Jacek Wysocki")
