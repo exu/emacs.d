b5daf07184c9ddb2b451babcfb4174e075ef5ce0
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-10-09 10:57:38 +0200

    notmuch config

--- autoload/mail.el
+++ autoload/mail.el
@@ -1,3 +1,7 @@
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
+;; installed from sources
+(require 'notmuch)
+
 (setq notmuch-message-headers-visible nil)
 (setq notmuch-fcc-dirs '((".*" . "SENT")))
 
@@ -9,6 +13,11 @@
 (require 'bbdb-vcard)
 
 ;; (add-hook 'mail-setup-hook 'bbdb-insinuate-
+
+
+(defun notmuch-get-unread-count ()
+  (number-to-string(string-to-number (shell-command-to-string (concat notmuch-command " search tag:unread|wc -l"))))
+  )
 
 
 (define-key notmuch-search-mode-map "e"
@@ -25,3 +34,6 @@
     (interactive)
     (notmuch-search-tag-all '("-inbox" "-unread"))
     ))
+
+
+(add-to-list 'mode-line-format (notmuch-get-unread-count))
