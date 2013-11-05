fbe08ccc3e12d25483e1b8f16ce5d7bf4ada2832
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-11-05 08:35:04 +0100

    added visual-regexp and python regexp backend

---
+++ autoload/regexp.el
@@ -0,0 +1,6 @@
+(require 'visual-regexp-steroids)
+(define-key global-map (kbd "C-c r") 'vr/replace)
+(define-key global-map (kbd "C-c q") 'vr/query-replace)
+;; to use visual-regexp's isearch instead of the built-in regexp isearch, also include the following lines:
+(define-key esc-map (kbd "C-r") 'vr/isearch-backward) ;; C-M-r
+(define-key esc-map (kbd "C-s") 'vr/isearch-forward) ;; C-M-s
