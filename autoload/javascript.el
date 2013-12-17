9326f753e6d1932d457926575110b1cda584233e
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-12-17 17:34:18 +0100

    Grunt file handling

--- autoload/javascript.el
+++ autoload/javascript.el
@@ -1,6 +1,9 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("Gruntfile" . js2-mode))
+
+
 
 ;; (add-to-list 'load-path "~/.emacs.d/vendor/tern/emacs/")
 ;; (autoload 'tern-mode "tern.el" nil t)
