568d795b179ed17daad301b8aaacca711459a8c8
Jacek Wysocki <jacek.wysocki@gmail.com>
2014-01-14 06:36:32 +0100

    more go

--- autoload/go.el
+++ autoload/go.el
@@ -4,11 +4,19 @@
 (setenv "GOOS" "linux")
 (setenv "GOARCH" "amd64")
 (setenv "GOPATH" "~/go")
-(setenv "PATH" (concat (getenv "PATH") ":" "~/Appz/go/pkg/linux_amd64"))
+(setenv "PATH" (concat (getenv "PATH") ":" "~/go/pkg/linux_amd64" ":" "~/go/bin/"))
 
 ;; (require 'go-koans)
 (require 'go-mode-load)
 (add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake"))
 
+(setq exec-path (append exec-path (list (expand-file-name "~/go/bin/"))))
+(setq exec-path (append exec-path (list (expand-file-name "~/.gvm/gos/go1.1/bin/"))))
+
 ;; https://github.com/dougm/goflymake install needed
 (require 'go-flymake)
+
+
+;; https://github.com/nsf/gocode
+(require 'go-autocomplete)
+(require 'auto-complete-config)
