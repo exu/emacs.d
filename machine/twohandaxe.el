b4b7461cb56e9f8c4c910732f44255bb759b45d2
Jacek Wysocki <jacek.wysocki@gmail.com>
2014-02-21 12:10:58 +0100

    I'm going to do some mess in ws project

--- machine/twohandaxe.el
+++ machine/twohandaxe.el
@@ -1,4 +1,4 @@
-;;; (gpicker-visit-project "~/www/sf2/tnowak")
+(gpicker-visit-project "~/www/websites")
 
 
 (global-set-key (kbd "<C-f10>") ( lambda ()
@@ -15,8 +15,9 @@
 
 
 
-(global-set-key (kbd "<f1>1") (lambda nil (interactive) (cd-and-visit "~/www/websites") ))
+(global-set-key (kbd "<f1>1") (lambda nil (interactive) (cd-and-visit "~/www/websites") (magit-status) ))
 (global-set-key (kbd "<f1>2") (lambda nil (interactive) (cd-and-visit "~/www/crm") ))
+
 (global-set-key (kbd "<f1>3") (lambda nil (interactive) (cd-and-visit "~/www/iq-api-client") ))
 (global-set-key (kbd "<f1>4") (lambda nil (interactive) (cd-and-visit "~/www/crm-api-client") ))
 (global-set-key (kbd "<f1>5") (lambda nil (interactive) (cd-and-visit "~/www/deployer") ))
