6a67becd60d883bdb5d51635d3a5ad07b6de16cf
Jacek Wysocki <jacek.wysocki@pearson.com>
2018-04-18 08:37:56 +0200

    added melpa

---
+++ init.el
@@ -0,0 +1,17 @@
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
